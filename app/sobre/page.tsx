2d040045cbf55db883e40f5d269c48697eed1fa4
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2025-01-10 09:50:42 -0300

    Update page.tsx

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -6,6 +6,22 @@ const Sobre: React.FC = () => {
     <div>
       <h1>Sobre Nós</h1>
       <p>Informações sobre a empresa...</p>
+      <h2>Sobre o gerador de link curto Wzapp.click</h2>
+      <p>
+        Criado em 2025, o Wzapp.click é uma ferramenta online e gratuita para
+        qualquer pessoa ou negócio. O site existe para encurtar o caminho do seu
+        amigo/cliente com você, com um link personalizado que leva o usuário
+        direto para seu WhatsApp além de trazer um blog de tecnologia incluso,
+        trazendo novidades do mundo digital.
+      </p>
+      <p>
+        Todos os conteúdos e dados deste site são apenas para fins informativos,
+        não devem ser considerados completos, atualizados, e não se destinam a
+        ser utilizado no lugar de uma consulta jurídica, médica, financeira, ou
+        de qualquer outro profissional. Os conteúdos são fornecidos sem qualquer
+        tipo de garantia. Todo e qualquer risco da utilização dos conteúdos é
+        assumido pelo próprio usuário.
+      </p>
     </div>
   );
 };
