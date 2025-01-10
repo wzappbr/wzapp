5f500a85e3cccd8b44077575b5b49f01cf16c4bd
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2025-01-10 09:39:18 -0300

    ok

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,13 +8,15 @@ const Header: React.FC = () => {
     <header className="header">
       <div className="header-container">
         <Link href="/">
-          <Image
-            src="https://i.imgur.com/2L7C7zH.png"
-            alt="Logo da Empresa"
-            width={300}
-            height={80}
-            className="logo"
-          />
+          <div className="logo-container">
+            <Image
+              src="https://i.imgur.com/2L7C7zH.png"
+              alt="Logo da Empresa"
+              fill
+              className="logo"
+              style={{ objectFit: "contain" }} // Mantém a proporção
+            />
+          </div>
         </Link>
       </div>
     </header>
